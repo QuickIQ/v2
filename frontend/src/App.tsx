2acde3e319fda318b8b2e4deec09adfccf2c890b
QuickIQ <ceyhun_3@hotmail.com>
2025-11-15 15:41:02 +0300

    Backup: Before cleanup - backup files and old directories

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import { loadStripe } from '@stripe/stripe-js';
 import i18n from './i18n/config';
 import { Header } from './components/layout/Header';
 import Home from './pages/Home';
-import TestFlow from './pages/TestFlow';
 import IQTestPage from './pages/IQTestPage';
 import PersonalityTestPage from './pages/PersonalityTestPage';
 import CreativeThinkingTestPage from './pages/CreativeThinkingTestPage';
@@ -87,7 +86,6 @@ function AppContent() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/test/:slug" element={<TestFlow />} />
         <Route path="/test/iqtest" element={<IQTestPage />} />
         <Route path="/test/personality" element={<PersonalityTestPage />} />
         <Route path="/test/personality/payment" element={<PaymentPage />} />
